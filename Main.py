bfe291053c1c458ca8c161cb30c2d6f50bcea461
Rajvir Vyas <rajvirvyas23@gmail.com>
2023-11-13 08:41:11 -0800

    readded dice

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from board import Board
 from graphics import *
-
+from dice import *
+from tkinter import *
 
 def main():
     win = GraphWin("My Circle", 1000, 700)
@@ -11,13 +12,19 @@ def main():
     circle: Circle = Circle(Point(0.0, 0.0), 20.0)
     circle.setFill("Red")
     circle.draw(win)
+    # For Dice-----------------------------------------------------------------------------
+    l1 = Label(win, font=("Helvetica", 150), text='')  # Create a label with empty text
+    l1.place(x=800, y=0)
+    b1 = Button(win, text="Roll the Dice!", foreground='blue', command=lambda: roll(l1))
+    b1.place(x=800, y=0)
+#--------------------------------------------------------------------------------------
 
     while win.checkMouse() is None:
         x = win.winfo_pointerx()
         y = win.winfo_pointery()
         abs_coord_x = x - win.winfo_rootx()
         abs_coord_y = y - win.winfo_rooty()
-        print(f"x: {circle.getCenter().x - abs_coord_x}, y: {circle.getCenter().y - abs_coord_y}")
+        
         new_point: Point = board.bg.nearest_vertex(Point(abs_coord_x, abs_coord_y))
 
         circle.move(new_point.x - circle.getCenter().x, new_point.y - circle.getCenter().y)
